37e8695f6ac745970c789eb0c777f6e6f9361280
Mythri Alle <mythria@google.com>
2022-05-12 15:08:43 +0000

    Remove unused this in method unwind callback
    This object isn't used in any of the receivers and it is not easy to get
    this for quick frames without instrumentation stack. When there is a
    real need we can implement a way to get this object.
    
    Bug: 206029744
    Test: art/test.py
    Change-Id: I2c74920ee37cc787978c7ce759cf1be5f6b15e6d

--- runtime/interpreter/interpreter.cc
+++ runtime/interpreter/interpreter.cc
@@ -287,7 +287,6 @@ static inline JValue Execute(
       }
       if (UNLIKELY(self->IsExceptionPending())) {
         instrumentation->MethodUnwindEvent(self,
-                                           shadow_frame.GetThisObject(accessor.InsSize()),
                                            method,
                                            0);
         JValue ret = JValue();
