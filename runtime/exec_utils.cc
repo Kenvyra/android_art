hopez13/art

Fix performance-inefficient-vector-operation clang-tidy issues
Test: m tidy-art
Bug: 264654008
Change-Id: Ic0a52fb2c6842d18b292088135a39d6886b69d46

runtime/exec_utils.cc
@@ -68,6 +68,7 @@ pid_t ExecWithoutWait(const std::vector<std::string>& arg_vector, std::string* e
   // Convert the args to char pointers.
   const char* program = arg_vector[0].c_str();
   std::vector<char*> args;
+  args.reserve(args.size() + 1);
   for (const auto& arg : arg_vector) {
     args.push_back(const_cast<char*>(arg.c_str()));
   }

Kenvyra/android_art

Merged-In: I6b0b495cbb934f9b8caedb389a643428d3cffc47
(cherry picked from commit d59d4d116f9d40113c32c8b1225388308830b099)

runtime/exec_utils.cc
@@ -44,6 +44,7 @@ pid_t ExecWithoutWait(std::vector<std::string>& arg_vector) {
   // Convert the args to char pointers.
   const char* program = arg_vector[0].c_str();
   std::vector<char*> args;
+  args.reserve(arg_vector.size() + 1);
   for (const auto& arg : arg_vector) {
     args.push_back(const_cast<char*>(arg.c_str()));
   }