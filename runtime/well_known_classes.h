hopez13/art

Add helpers for classes from well known fields/methods.
Test: m test-art-host-gtest
Test: testrunner.py --host --optimizing
Change-Id: I91bdaf25bc17d9d35288aa7d7f1eb01da14850a9

runtime/well_known_classes.h
@@ -20,6 +20,7 @@
 #include "base/locks.h"
 #include "jni.h"
 #include "obj_ptr.h"
+#include "read_barrier_option.h"
 
 namespace art {
 
@@ -29,6 +30,34 @@ class ArtMethod;
 namespace mirror {
 class Class;
 }  // namespace mirror
+
+namespace detail {
+
+template <typename MemberType, MemberType** kMember>
+struct ClassFromMember {
+  template <ReadBarrierOption kReadBarrierOption = kWithReadBarrier>
+  static ObjPtr<mirror::Class> Get() REQUIRES_SHARED(Locks::mutator_lock_);
+
+  mirror::Class* operator->() const REQUIRES_SHARED(Locks::mutator_lock_);
+};
+
+template <typename MemberType, MemberType** kMember>
+bool operator==(const ClassFromMember<MemberType, kMember> lhs, ObjPtr<mirror::Class> rhs)
+    REQUIRES_SHARED(Locks::mutator_lock_);
+
+template <typename MemberType, MemberType** kMember>
+bool operator==(ObjPtr<mirror::Class> lhs, const ClassFromMember<MemberType, kMember> rhs)
+    REQUIRES_SHARED(Locks::mutator_lock_);
+
+template <typename MemberType, MemberType** kMember>
+bool operator!=(const ClassFromMember<MemberType, kMember> lhs, ObjPtr<mirror::Class> rhs)
+    REQUIRES_SHARED(Locks::mutator_lock_);
+
+template <typename MemberType, MemberType** kMember>
+bool operator!=(ObjPtr<mirror::Class> lhs, const ClassFromMember<MemberType, kMember> rhs)
+    REQUIRES_SHARED(Locks::mutator_lock_);
+
+}  // namespace detail
 
 // Various classes used in JNI. We cache them so we don't have to keep looking them up.
 
@@ -54,6 +83,12 @@ struct WellKnownClasses {
  private:
   static void InitFieldsAndMethodsOnly(JNIEnv* env);
 
+  template <ArtMethod** kMethod>
+  using ClassFromMethod = detail::ClassFromMember<ArtMethod, kMethod>;
+
+  template <ArtField** kField>
+  using ClassFromField = detail::ClassFromMember<ArtField, kField>;
+
  public:
   static jclass dalvik_annotation_optimization_CriticalNative;
   static jclass dalvik_annotation_optimization_FastNative;
@@ -62,9 +97,6 @@ struct WellKnownClasses {
   static jclass dalvik_system_BaseDexClassLoader;
   static jclass dalvik_system_DelegateLastClassLoader;
   static jclass dalvik_system_DexClassLoader;
-  static jclass dalvik_system_DexFile;
-  static jclass dalvik_system_DexPathList;
-  static jclass dalvik_system_DexPathList__Element;
   static jclass dalvik_system_EmulatedStackFrame;
   static jclass dalvik_system_InMemoryDexClassLoader;
   static jclass dalvik_system_PathClassLoader;
@@ -87,7 +119,6 @@ struct WellKnownClasses {
   static jclass java_lang_String;
   static jclass java_lang_StringFactory;
   static jclass java_lang_System;
-  static jclass java_lang_Throwable;
   static jclass java_lang_Void;
   static jclass libcore_reflect_AnnotationMember__array;
 
@@ -178,6 +209,18 @@ struct WellKnownClasses {
   static ArtField* org_apache_harmony_dalvik_ddmc_Chunk_length;
   static ArtField* org_apache_harmony_dalvik_ddmc_Chunk_offset;
   static ArtField* org_apache_harmony_dalvik_ddmc_Chunk_type;
+
+  static constexpr ClassFromField<&dalvik_system_DexFile_cookie> dalvik_system_DexFile;
+  static constexpr ClassFromField<&dalvik_system_DexPathList_dexElements> dalvik_system_DexPathList;
+  static constexpr ClassFromField<&dalvik_system_DexPathList__Element_dexFile>
+      dalvik_system_DexPathList__Element;
+  static constexpr ClassFromMethod<&java_lang_Thread_init> java_lang_Thread;
+  static constexpr ClassFromMethod<&java_lang_ThreadGroup_add> java_lang_ThreadGroup;
+  static constexpr ClassFromField<&java_lang_Throwable_cause> java_lang_Throwable;
+  static constexpr ClassFromField<&java_nio_Buffer_address> java_nio_Buffer;
+  static constexpr ClassFromField<&java_util_Collections_EMPTY_LIST> java_util_Collections;
+  static constexpr ClassFromField<&libcore_util_EmptyArray_STACK_TRACE_ELEMENT>
+      libcore_util_EmptyArray;
 };
 
 }  // namespace art

Kenvyra/android_art

runtime/well_known_classes.h
@@ -20,6 +20,7 @@
 #include "base/locks.h"
 #include "jni.h"
 #include "obj_ptr.h"
+#include "read_barrier_option.h"
 
 namespace art {
 
@@ -29,6 +30,34 @@ class ArtMethod;
 namespace mirror {
 class Class;
 }  // namespace mirror
+
+namespace detail {
+
+template <typename MemberType, MemberType** kMember>
+struct ClassFromMember {
+  template <ReadBarrierOption kReadBarrierOption = kWithReadBarrier>
+  static ObjPtr<mirror::Class> Get() REQUIRES_SHARED(Locks::mutator_lock_);
+
+  mirror::Class* operator->() const REQUIRES_SHARED(Locks::mutator_lock_);
+};
+
+template <typename MemberType, MemberType** kMember>
+bool operator==(const ClassFromMember<MemberType, kMember> lhs, ObjPtr<mirror::Class> rhs)
+    REQUIRES_SHARED(Locks::mutator_lock_);
+
+template <typename MemberType, MemberType** kMember>
+bool operator==(ObjPtr<mirror::Class> lhs, const ClassFromMember<MemberType, kMember> rhs)
+    REQUIRES_SHARED(Locks::mutator_lock_);
+
+template <typename MemberType, MemberType** kMember>
+bool operator!=(const ClassFromMember<MemberType, kMember> lhs, ObjPtr<mirror::Class> rhs)
+    REQUIRES_SHARED(Locks::mutator_lock_);
+
+template <typename MemberType, MemberType** kMember>
+bool operator!=(ObjPtr<mirror::Class> lhs, const ClassFromMember<MemberType, kMember> rhs)
+    REQUIRES_SHARED(Locks::mutator_lock_);
+
+}  // namespace detail
 
 // Various classes used in JNI. We cache them so we don't have to keep looking them up.
 
@@ -54,6 +83,12 @@ struct WellKnownClasses {
  private:
   static void InitFieldsAndMethodsOnly(JNIEnv* env);
 
+  template <ArtMethod** kMethod>
+  using ClassFromMethod = detail::ClassFromMember<ArtMethod, kMethod>;
+
+  template <ArtField** kField>
+  using ClassFromField = detail::ClassFromMember<ArtField, kField>;
+
  public:
   static jclass dalvik_annotation_optimization_CriticalNative;
   static jclass dalvik_annotation_optimization_FastNative;
@@ -62,9 +97,6 @@ struct WellKnownClasses {
   static jclass dalvik_system_BaseDexClassLoader;
   static jclass dalvik_system_DelegateLastClassLoader;
   static jclass dalvik_system_DexClassLoader;
-  static jclass dalvik_system_DexFile;
-  static jclass dalvik_system_DexPathList;
-  static jclass dalvik_system_DexPathList__Element;
   static jclass dalvik_system_EmulatedStackFrame;
   static jclass dalvik_system_InMemoryDexClassLoader;
   static jclass dalvik_system_PathClassLoader;
@@ -87,7 +119,6 @@ struct WellKnownClasses {
   static jclass java_lang_String;
   static jclass java_lang_StringFactory;
   static jclass java_lang_System;
-  static jclass java_lang_Throwable;
   static jclass java_lang_Void;
   static jclass libcore_reflect_AnnotationMember__array;
 
@@ -178,6 +209,18 @@ struct WellKnownClasses {
   static ArtField* org_apache_harmony_dalvik_ddmc_Chunk_length;
   static ArtField* org_apache_harmony_dalvik_ddmc_Chunk_offset;
   static ArtField* org_apache_harmony_dalvik_ddmc_Chunk_type;
+
+  static constexpr ClassFromField<&dalvik_system_DexFile_cookie> dalvik_system_DexFile;
+  static constexpr ClassFromField<&dalvik_system_DexPathList_dexElements> dalvik_system_DexPathList;
+  static constexpr ClassFromField<&dalvik_system_DexPathList__Element_dexFile>
+      dalvik_system_DexPathList__Element;
+  static constexpr ClassFromField<&java_lang_Thread_daemon> java_lang_Thread;
+  static constexpr ClassFromField<&java_lang_ThreadGroup_groups> java_lang_ThreadGroup;
+  static constexpr ClassFromField<&java_lang_Throwable_cause> java_lang_Throwable;
+  static constexpr ClassFromField<&java_nio_Buffer_address> java_nio_Buffer;
+  static constexpr ClassFromField<&java_util_Collections_EMPTY_LIST> java_util_Collections;
+  static constexpr ClassFromField<&libcore_util_EmptyArray_STACK_TRACE_ELEMENT>
+      libcore_util_EmptyArray;
 };
 
 }  // namespace art